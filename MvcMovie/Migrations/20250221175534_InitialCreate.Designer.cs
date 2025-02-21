38c4aeb85203655dc08894f7b9e3945b4ec3eb2f
Emil Motrøen <e.motroen@gmail.com>
2025-02-21 18:56:48 +0100

    Add part 8

--- MvcMovie/Migrations/20250221175534_InitialCreate.Designer.cs
+++ MvcMovie/Migrations/20250221175534_InitialCreate.Designer.cs
@@ -11,7 +11,7 @@ using MvcMovie.Data;
 namespace MvcMovie.Migrations
 {
     [DbContext(typeof(MvcMovieContext))]
-    [Migration("20250204140322_InitialCreate")]
+    [Migration("20250221175534_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
@@ -30,6 +30,9 @@ namespace MvcMovie.Migrations
                         .HasColumnType("TEXT");
 
                     b.Property<decimal>("Price")
+                        .HasColumnType("decimal(18, 2)");
+
+                    b.Property<string>("Rating")
                         .HasColumnType("TEXT");
 
                     b.Property<DateTime>("ReleaseDate")
